06ada540b029d9e7fb8759edece5e43a924f4d12
chenguiqi <chenguiqi@dashuf.com>
2018-09-27 10:40:42 +0800

    1

---
+++ RibbonConsumers/src/main/java/com/exercise/consumers/App.java
@@ -0,0 +1,27 @@
+package com.exercise.consumers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@EnableDiscoveryClient
+@EnableCircuitBreaker
+@SpringBootApplication
+public class App 
+{
+	@Bean
+	@LoadBalanced
+	RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
+	
+    public static void main(String[] args )
+    {
+        SpringApplication.run(App.class, args);
+    }
+}
